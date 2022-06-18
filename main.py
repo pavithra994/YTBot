ab5a7810544929440f90af80cafbe4722b1a8405
pavithra <pavithra.blog@gmail.com>
2022-06-18 18:51:38 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+import json
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/channel_subscribe/{channel_id}")
+async def channel_subscribe(channel_id: int):
+    new_channel =  {"channel_id": channel_id}
+    with open('data.json') as f:
+        data = json.load(f)
+
+    channel_list = data['channel_data']
+    channel_list.append(new_channel)
+    data['channel_data'] = channel_list
+
+    with open('data.json', 'w') as f:
+        json.dump(data, f, indent=2)
+
+    return new_channel
